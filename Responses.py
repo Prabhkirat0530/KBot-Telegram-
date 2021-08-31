1c2e827e4d8418cc2241bf28a2eff9b08212d948
Prabhkirat <prabhkirat05@gmail.com>
2021-08-31 20:20:52 +0530

    Python Telegram Bot

---
+++ Responses.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def sample_responses(input_text):
+    user_message = str(input_text).lower()
+
+    if user_message in ("hello", "hi", "hey", "yo",):
+        return "Hey! What's up?"
+
+    if user_message in ("who are you", "who are you?",):
+        return "I am KBot"
+
+    if user_message in ("time", "time?",):
+        now = datetime.now()
+        date_time = now.strftime("%d/%m/%y, %H:%M:%S")
+        return str(date_time)
+
+    return "I don't know what you said."
